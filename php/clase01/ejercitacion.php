2f18fe56d33ca36f7ff6cbf2c9d68ef5cadd8e3c
Julian <Julian_Boschi@yahoo.com.ar>
2019-04-08 22:04:40 -0300

    Clase 1

---
+++ php/clase01/ejercitacion.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    echo $numero = 5;
+    echo "<hr>";
+    echo $numerodecimal = 5.5;
+    echo"<hr>";
+    echo $nombre= 'Julian';
+    echo"<hr>";
+    echo $apellido= "Boschi";
+    echo"<hr>";
+    echo $numero= "hola que tal";
+    echo"<hr>";
+    echo $nombre= 5.2;
+    echo"<hr>";
+    echo $numero = "5.6";
+
+
+    $uno = "tres";
+    $dos = "tristes";
+    $tres  = "tigres";
+    $cuatro = "tragan";
+    $cinco = "trigo" ;
+    $seis = "en";
+    $siete = "un";
+    $ocho = "trigal";
+
+    echo $uno. "<br>" .$dos ."<br>" .$tres ."<br>" .$cuatro ."<br>" .$cinco . "<br>" .$seis ."<br>" .$siete ."<br>" .$ocho;
+
+    var_dump($uno);
+
+
+     ?>
+
+  </body>
+</html>
