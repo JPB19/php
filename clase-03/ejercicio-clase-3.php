340fccd93526e478d1442e445d091c7e2cd46ebe
JPB19 <Julian_Boschi@yahoo.com.ar>
2019-04-22 17:32:49 -0300

    subo ejecrcicio clase 3 terminado y ejercicio clase 5 super incompleto

--- clase-03/ejercicio-clase-3.php
+++ clase-03/ejercicio-clase-3.php
@@ -169,15 +169,112 @@ do {
 echo "<br><hr>";
 
 $arrayLetras = range("a", "o");
-
+$value = 0;
 for ($i=0; $i < count($arrayLetras); $i++) {
-  echo 
+  $value = $arrayLetras[$i];
+  echo "En la posicion $i se encuentra el valor" ." " . $value . " " . "donde" ." " . $value . " " . "se reemplazara por cada valor del array y $i representa el indice";
+}
+
+echo "<br><hr>";
+
+
+$mascota = [
+
+  "animal" => "perro",
+  "edad" => 6,
+  "altura" => "30cm",
+  "nombre" => "rocky",
+
+];
+
+foreach ($mascota as $datos => $valor) {
+  echo "$datos : $valor <br>" ;
+}
+
+echo "<br><hr>";
+
+$ceu = array( "Italia"=>"Roma", "Luxembourg"=>"Luxembourg", "Bélgica"=> "Bruselas",
+"Dinamarca"=>"Copenhagen", "Finlandia"=>"Helsinki", "Francia" => "Paris",
+"Slovakia"=>"Bratislava", "Eslovenia"=>"Ljubljana", "Alemania" => "Berlin", "Grecia" => "Athenas",
+"Irlanda"=>"Dublin", "Holanda"=>"Amsterdam", "Portugal"=>"Lisbon", "España"=>"Madrid",
+"Suecia"=>"Stockholm", "Reino Unido"=>"London", "Chipre"=>"Nicosia", "Lithuania"=>"Vilnius",
+"Republica Checa"=>"Prague", "Estonia"=>"Tallin", "Hungría"=>"Budapest", "Latvia"=>"Riga",
+"Malta"=>"Valletta", "Austria" => "Vienna", "Polonia"=>"Warsaw") ;
+
+
+foreach ($ceu as $pais => $capital) {
+  echo "$pais : $capital <br>";
+}
+echo "<br><hr>";
+
+$ceu1 = [
+"Argentina" => ["Buenos Aires", "Córdoba", "Santa Fé"],
+"Brasil" => ["Brasilia", "Rio de Janeiro", "Sao Pablo"],
+"Colombia" => ["Cartagena", "Bogota", "Barranquilla"],
+"Francia" => ["Paris", "Nantes", "Lyon"],
+"Italia" => ["Roma", "Milan", "Venecia"],
+"Alemania" => ["Munich", "Berlin", "Frankfurt"]
+];
+
+foreach ($ceu1 as $pais => $ciudades) {
+echo "las ciudades de $pais son";
+echo "<ul>";
+foreach ($ciudades as $ciudad => $valor) {
+  echo "<li> $valor</li>";
+}
+echo "</ul>";
+
 }
 
 
 
 
 
+
+
+
+
+echo "<br><hr>";
+
+
+$paises = [
+  "Argentina" => [
+    "esAmericano" => true,
+    "ciudades" => ["Buenos Aires", "Córdoba"]
+  ],
+  "Brasil" => [
+    "esAmericano" => true,
+    "ciudades" => ["Brasilia", "Rio de Janeiro"]
+  ],
+  "Francia" => [
+    "esAmericano" => false,
+    "ciudades" =>  ["Paris", "Nantes", "Lyon"]
+  ],
+  "Italia" => [
+    "esAmericano" => false,
+    "ciudades" =>  ["Roma", "Milan", "Venecia"]
+  ],
+  "Alemania" => [
+    "esAmericano" => false,
+    "ciudades" =>  ["Munich", "Berlin", "Frankfurt"]
+  ],
+  "Colombia" => [
+    "esAmericano" => true,
+    "ciudades" => ["Cartagena", "Bogota", "Barranquilla"]
+  ],
+];
+
+foreach ($paises as $pais => $datos) {
+  if ($datos["esAmericano"] === true) {
+    echo "Las ciudades de $pais son: <br>";
+      echo "<ul>";
+      foreach ($datos["ciudades"] as $ciudad) {
+        echo "<li> $ciudad </li>";
+      }
+      echo "</ul>";
+    }
+
+  }
 
 
 
---
+++ clase-05/imprimir.php
@@ -0,0 +1,10 @@
+<?php
+
+
+var_dump($_GET);
+
+echo $_GET["nombre"];
+echo "<br><hr>";
+echo $_GET["email"];
+
+ ?>
